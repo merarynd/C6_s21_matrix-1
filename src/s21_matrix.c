9c9f01ea12b3cbea9b61e50478c24d38b0fa208c
Mera Ryndoon <merarynd@pr-i2.kzn.21-school.ru>
2023-04-30 22:05:38 +0300

    start

---
+++ src/s21_matrix.c
@@ -0,0 +1,10 @@
+#include "s21_matrix.h"
+
+int s21_create_matrix(int rows, int columns, matrix_t *result) {
+  if (rows > 0 && columns > 0) {
+    for (int i; i != rows; i++) {
+      for (int j; j != columns; i++) {
+      }
+    }
+  }
+}
