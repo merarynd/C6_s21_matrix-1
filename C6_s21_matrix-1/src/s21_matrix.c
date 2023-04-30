6b83ec87db9a799aaad2a36118299b24afa5ddd2
merarynd <118696716+merarynd@users.noreply.github.com>
2023-04-30 20:12:25 +0300

    Add files via upload

---
+++ C6_s21_matrix-1/src/s21_matrix.c
@@ -0,0 +1,13 @@
+#include "s21_matrix.h"
+
+int s21_create_matrix() {}
+// int s21_remove_matrix() {}
+// int s21_eq_matrix() {}
+// int s21_sum_matrix() {}
+// int s21_sub_matrix() {}
+// int s21_mult_matrix() {}
+// int s21_mult_number() {}
+// int s21_transpose() {}
+// int s21_determinant() {}
+// int s21_calc_complements() {}
+// int s21_inverse_matrix() {}
---
+++ C6_s21_matrix-1/src/s21_matrix.h
@@ -0,0 +1,24 @@
+#ifndef S21_MATRIX_H_
+#define S21_MATRIX_H_
+
+#include <limits.h>
+#include <math.h>
+#include <stdbool.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+// int s21_create_matrix();
+// int s21_remove_matrix();
+// int s21_eq_matrix();
+// int s21_sum_matrix();
+// int s21_sub_matrix();
+// int s21_mult_matrix();
+// int s21_mult_number();
+// int s21_transpose();
+// int s21_determinant();
+// int s21_calc_complements();
+// int s21_inverse_matrix();
+
+#endif  // S21_MATRIX_H_
